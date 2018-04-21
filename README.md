c6eb27e391763993b58d0055ef9c6d9871a394ca
Tino <tino@solarmonkey.nl>
2018-04-21 10:19:33 +0100

    Add endpoints to reveal secrets

--- README.md
+++ README.md
@@ -3,5 +3,9 @@
 # TODO
 
 - [x] Add secret to share api
+- [x] Return share url after adding
+- [x] Endpoint that shows secret preview
+- [x] Endpoint to show the secret
+- [ ] UI page
 - [ ] Add form to post secret
 - [ ] A lot more...
--- secrets.py
+++ secrets.py
@@ -1,3 +1,4 @@
+import datetime
 import os
 
 import hvac
@@ -6,6 +7,8 @@ from apistar import App, Route, exceptions, types, validators
 import settings
 from utils import slugify
 from vault import master_client
+
+CUBBYHOLE_PATH = path = os.path.join(settings.VAULT_SECRET_BASE, 'secret')
 
 
 class Secret(types.Type):
@@ -20,8 +23,7 @@ def new_cubbyhole(secret: Secret):
     """
     token = master_client.create_token(policies=['single-secure-share'], lease='168h')
     client = hvac.Client(settings.VAULT_URL, token=token['auth']['client_token'])
-    path = os.path.join(settings.VAULT_SECRET_BASE, slugify(secret.name))
-    client.write(path, name=secret.name, value=secret.value, lease=f'{7 * 24}h')
+    client.write(CUBBYHOLE_PATH, name=secret.name, value=secret.value, lease=f'{7 * 24}h')
     return token
 
 
@@ -31,8 +33,29 @@ def new_secret_page(app: App):
 
 def new_secret(app: App, secret: Secret):
     token = new_cubbyhole(secret)
-    return {'token': token}
-    # raise exceptions.Found(location=app.reverse_url('secrets:secret_added'))
+    return {
+        'url': app.reverse_url('secrets:show_secret', token=token['auth']['client_token']),
+        'expiration': (
+            datetime.datetime.now() +
+            datetime.timedelta(seconds=token['auth']['lease_duration'])
+        ).isoformat()
+    }
+
+
+def show_secret(token: str):
+    client = hvac.Client(settings.VAULT_URL, token=token)
+    try:
+        return client.lookup_token()
+    except hvac.exceptions.Forbidden:
+        raise exceptions.NotFound
+
+
+def show_secret_contents(token: str):
+    client = hvac.Client(settings.VAULT_URL, token=token)
+    try:
+        return client.read(CUBBYHOLE_PATH)
+    except hvac.exceptions.Forbidden:
+        raise exceptions.NotFound
 
 
 def secret_added(app: App):
@@ -43,4 +66,6 @@ routes = [
     Route('/new', method='GET', handler=new_secret_page),
     Route('/new', method='POST', handler=new_secret),
     Route('/added', method='GET', handler=secret_added),
+    Route('/show/{token}', method='GET', handler=show_secret),
+    Route('/show/{token}/content', method='GET', handler=show_secret_contents),
 ]
