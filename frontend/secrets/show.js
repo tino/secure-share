6dad79ea3200b4643b224b558d540478ee4816c8
Tino <tino@solarmonkey.nl>
2018-05-13 19:05:25 +0200

    Make show a bit nicer

--- frontend/secrets/show.js
+++ frontend/secrets/show.js
@@ -18,13 +18,42 @@ export const Show = ({ showState, showActions }) => ({ match }) => (
             <dt>Name</dt>
             <dd>{showState.secret.data.name}</dd>
             <dt>Value</dt>
-            <dd>{showState.secret.data.value}</dd>
+            <dd>
+              <div class="columns">
+                <div class="column is-10">
+                  <div class="control">
+                    <input
+                      value={showState.secret.data.value}
+                      class="input is-medium"
+                      onclick={e => e.target.select()}
+                      oncreate={e => e.select()}
+                      id="secret"
+                    />
+                  </div>
+                </div>
+                <div class="column is-2">
+                  <button
+                    onclick={e => {
+                      document.querySelector("#secret").select()
+                      document.execCommand("copy")
+                    }}
+                    class="button is-medium is-white is-outlined"
+                  >
+                    <span>Copy</span>
+                  </button>
+                </div>
+              </div>
+            </dd>
           </dl>
         )}
         {showState && !showState.data && !showState.error && <p>Loading...</p>}
         {showState.error && <p>Whoops! {showState.error}</p>}
-        {!showState.secret && <button onclick={() => showActions.reveal()}>Reveal</button>}
-        {/* <pre>{JSON.stringify(showState)}</pre> */}
+        {showState.data &&
+          !showState.secret && (
+            <button class="button is-medium is-danger" onclick={() => showActions.reveal()}>
+              Reveal
+            </button>
+          )}
       </div>
     </div>
   </section>
